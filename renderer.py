d8a842924eac8df46ecc30a6dfce1251a85ca780
David Lerner <dmlerner@gmail.com>
2019-06-10 18:25:37 -0700

    outputs valid svg!

--- renderer.py
+++ renderer.py
@@ -2,30 +2,52 @@ import music21
 import threading
 import os
 import time
+import re
+import pdb
 
 extension = '.svg'
 
 class Renderer:
 
-    def __init__(self, stream=None, writer='lily'+extension, viewer='svg_refreshview.py', path='./piano'):
+    def __init__(self, stream=None, writer='lily.ly', viewer='svg_refreshview.py', path='sheetmusic'):
         self.stream = stream or music21.stream.Stream()
         self.writer = writer
         self.viewer = viewer
-        self.path = path #+ str(time.time())
-
-        '''
-        music21.environment.set('vectorPath', '/usr/bin/' + viewer)
-        music21.environment.set('musicxmlPath', '/usr/bin/' + viewer)
-        music21.environment.set('graphicsPath', '/usr/bin/' + viewer)
-        '''
-
+        self.path = path + str(time.time())
 
     def write(self):
         self.stream.write(self.writer, fp=self.path)
+        with open(self.path) as f:
+            ly_lines = f.readlines()
+        #pdb.set_trace()
+        print ''.join(ly_lines)
+        print '.'*20
+        self.remove_lily_header(ly_lines)
+        print ''.join(ly_lines)
+        print '.'*20
+        self.fix_colors(ly_lines)
+        print ''.join(ly_lines)
+        print
+        with open(self.path + '.ly', 'w') as f:
+            f.write(''.join(ly_lines))
+        os.system('lilypond -dbackend=svg ' + self.path + '.ly')
+
+    def remove_lily_header(self, ly_lines):
+        for i, line in enumerate(ly_lines):
+            if 'header' in line:
+                break
+        del ly_lines[:i+1]
+
+    def fix_colors(self, ly_lines):
+        for i, line in enumerate(ly_lines):
+            if '\color' in line:
+                parsed = head, r, g, b, tail = re.search(r'(.*)\color "(.*) (.*) (.*)" (.*)', line).groups()
+                ly_lines[i] = '%s \override NoteHead #\'color = #(rgb-color %s %s %s) %s' % (head[:-2], r, g, b, tail)
 
     def loop_write(self, frames=float('inf'), delay=0):
         frame = 0
         while frame < frames:
+            print 'frame ', frame
             frame += 1
             self.write()
             time.sleep(delay)
@@ -36,6 +58,7 @@ class Renderer:
         os.system(view_command)
 
     def animate(self, frames=float('inf'), delay=0):
+        return
         threading.Thread(target=self.loop_write, args=(frames, delay)).start()
         while not os.path.isfile(self.path + extension):
             print 'path not found ' + self.path + extension
@@ -44,15 +67,18 @@ class Renderer:
 
 def main():
     r = Renderer()
-    use_rgb = False
+    #use_rgb = True
+    use_rgb = True
     def toggle_color(note):
         for i in range(100):
             time.sleep(.2)
             #raw_input('enter to toggle color')
-            note.style.color = [['red', 'green'], ['#ff0000', '#00ff00']][use_rgb][i%2]
+            note.style.color = [['green1', 'green2'], ['.5 0 0', '0 .5 0']][use_rgb][i%2]
     note = music21.note.Note('F5')
+    note.style.color = ['blue', '0 0 .5'][use_rgb]
     r.stream.append(note)
-    note.style.color = ['blue', '#0000ff'][use_rgb]
+    r.write()
+    1/0
     threading.Thread(target=toggle_color, args=(note,)).start()
     r.animate()
 
